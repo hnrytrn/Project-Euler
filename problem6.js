f54ce44137293236d31f53e5421bf94a679466af
Henry Tran <22henrytran@gmail.com>
2016-06-02 20:53:56 -0400

    add problem 6 solution

---
+++ problem6.js
@@ -0,0 +1,19 @@
+/* The sum of the squares of the first ten natural numbers is,
+
+1^2 + 2^2 + ... + 10^2 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)^2 = 55^2 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum. */
+
+var sumOfSq = 0;
+var sum = 0;
+
+for (var i = 1; i <= 100; i++) {
+	sumOfSq += (i*i);
+	sum += i;
+}
+
+console.log("Difference = " + (sum*sum - sumOfSq));
